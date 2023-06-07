02ba212d331d5e09a9743329ede30084066dd589
Borys Mankowski <borys.mankowski@gmail.com>
2023-06-07 19:27:29 +0200

    fixed the controller tests for lessons

--- src/test/java/com/masters/coding/CodingApplicationTests.java
+++ src/test/java/com/masters/coding/CodingApplicationTests.java
@@ -2,8 +2,11 @@ package com.masters.coding;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 class CodingApplicationTests {
 
     @Test
